8206336285221dd22c2fc638d80300d71321646a
lemon <lemon@123.com>
2024-05-16 10:14:54 +0800

    差生文具多，先把依赖环境加上。

--- src/main/java/com/orange/orangepilotserver/OrangepilotserverApplication.java
+++ src/main/java/com/orange/orangepilotserver/OrangepilotserverApplication.java
@@ -1,9 +1,11 @@
 package com.orange.orangepilotserver;
 
+import io.swagger.v3.oas.models.annotations.OpenAPI31;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPI31
 public class OrangepilotserverApplication {
 
     public static void main(String[] args) {
